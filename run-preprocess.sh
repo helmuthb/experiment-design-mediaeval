28daa366e1fc825ea747c3a31c9db7d9beb1355e
László Király <laszlo.t.kiraly@gmail.com>
2020-02-01 18:14:59 +0100

    only run pythonX if exists

--- run-preprocess.sh
+++ run-preprocess.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
 rm -rf processed 2>/dev/null
-python3 preprocessing.py
+if hash python3 2>/dev/null; then
+    python3 preprocessing.py
+else
+    python preprocessing.py
+fi
--- split-train-test.sh
+++ split-train-test.sh
@@ -4,5 +4,10 @@ cd data
 for SOURCE in allmusic discogs lastfm tagtraum
 do
     FILE=acousticbrainz-mediaeval-${SOURCE}-train.tsv
-    python2 ../participant_split_data.py -m 0 $FILE
+    if hash python2 2>/dev/null; then
+        python2 ../participant_split_data.py -m 0 $FILE
+    else
+        python ../participant_split_data.py -m 0 $FILE
+    fi
+    
 done
