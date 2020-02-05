91b551462584e8fa187736a3784458d38b6d79db
László Király <laszlo.t.kiraly@gmail.com>
2020-02-05 22:48:23 +0100

    removed additional split and had to fix index_val/index_train

--- step1/common.py
+++ step1/common.py
@@ -7,7 +7,7 @@ DATASETS_DIR = DATA_DIR+"/splits"
 TRAINDATA_DIR = DATA_DIR+"/train_data"
 SAVED_MODELS_DIR = DATA_DIR+"/saved_models"
 
-def load_data_hf5_memory(dataset,val_percent, test_percent, y_path, id2gt):
+def load_data_hf5_memory(dataset, y_path, id2gt):
     hdf5_file = f"{PATCHES_DIR}/patches_train_{dataset}_1x1.hdf5"
     f = h5py.File(hdf5_file,"r")
     index_train = f["index"][:]
@@ -21,7 +21,7 @@ def load_data_hf5_memory(dataset,val_percent, test_percent, y_path, id2gt):
     factors_val = np.load(DATASETS_DIR+'/y_val_'+y_path+'.npy')
     index_factors_val = open(DATASETS_DIR+'/items_index_val_'+dataset+'.tsv').read().splitlines()
     id2gt_val = dict((index,factor) for (index,factor) in zip(index_factors_val,factors_val))
-    index_val = [i for i in f_val['index'][:] if i in id2gt_val]
+    index_val = [i.decode('utf-8') for i in f_val['index'][:] if i.decode('utf-8') in id2gt_val]
     X_val = np.delete(X_val, np.where(index_val == ""), axis=0)
     index_val = np.delete(index_val, np.where(index_val == ""))                
 
@@ -33,32 +33,26 @@ def load_data_hf5_memory(dataset,val_percent, test_percent, y_path, id2gt):
     factors_test = np.load(DATASETS_DIR+'/y_test_'+y_path+'.npy')
     index_factors_test = open(DATASETS_DIR+'/items_index_test_'+dataset+'.tsv').read().splitlines()
     id2gt_test = dict((index,factor) for (index,factor) in zip(index_factors_test,factors_test))
-    index_test = [i for i in f_test['index'][:] if i in id2gt_test]
+    index_test = [i.decode('utf-8') for i in f_test['index'][:] if i.decode('utf-8') in id2gt_test]
     X_test = np.delete(X_test, np.where(index_test == ""), axis=0)
     index_test = np.delete(index_test, np.where(index_test == ""))                
 
     Y_test = np.asarray([id2gt_test[id] for id in index_test])
-
-    hdf5_file = f"{PATCHES_DIR}/patches_train_{dataset}_1x1.hdf5"
-    f = h5py.File(hdf5_file,"r")
-    index_all = f["index"][:]
-    N = index_all.shape[0]
-    train_percent = 1 - val_percent - test_percent
-    N_train = int(train_percent * N)
-    N_val = int(val_percent * N)
-    X_val = f['features'][N_train:N_train+N_val]
-    index_val = f['index'][N_train:N_train+N_val]
-    X_val = np.delete(X_val, np.where(index_val == ""), axis=0)
-    index_val = np.delete(index_val, np.where(index_val == ""))              
-    Y_val = np.asarray([id2gt[id.decode('utf-8')] for id in index_val])
-    X_test = f['features'][N_train+N_val:N]
-    index_test = f['index'][N_train+N_val:N]
-    X_test = np.delete(X_test, np.where(index_test == ""), axis=0)
-    index_test = np.delete(index_test, np.where(index_test == ""))                
-    Y_test = np.asarray([id2gt[id.decode('utf-8')] for id in index_test])
-    index_train = f['index'][:N_train]
-    index_train = np.delete(index_train, np.where(index_train == ""))
-    N_train = index_train.shape[0]
     
     return X_val, Y_val, X_test, Y_test, N_train
 
+def load_data_preprocesed(params, X_path, Y_path, dataset, val_percent, test_percent, n_samples, metadata_source, with_metadata=False, only_metadata=False):
+    factors = np.load(DATASETS_DIR+'/y_train_'+Y_path+'.npy') # OJO remove S
+    all_Y = factors
+    all_X_meta = np.load(TRAINDATA_DIR+'/X_train_%s_%s.npy' % (metadata_source,dataset))
+    all_X = all_X_meta
+    print(all_X.shape)
+    print(all_Y.shape)
+    Y_val = np.load(DATASETS_DIR+'/y_val_'+Y_path+'.npy')
+    Y_test = np.load(DATASETS_DIR+'/y_test_'+Y_path+'.npy') #!!! OJO remove S from trainS
+    X_val = np.load(TRAINDATA_DIR+'/X_val_%s_%s.npy' % (metadata_source,dataset))
+    X_test = np.load(TRAINDATA_DIR+'/X_test_%s_%s.npy' % (metadata_source,dataset))
+    X_train = all_X
+    Y_train = all_Y
+
+    return X_train, Y_train, X_val, Y_val, X_test, Y_test
