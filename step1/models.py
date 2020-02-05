bd5475ccf0afe3d426cc6eee36774a781ee5f257
László Király <laszlo.t.kiraly@gmail.com>
2020-02-05 10:16:46 +0100

    subtask2 model draft

--- step1/models.py
+++ step1/models.py
@@ -27,3 +27,24 @@ class Subtask1Model(nn.Module):
     def get_activation(self, x):
         return F.relu(self.input(x))
 
+class Subtask2Model(nn.Module):
+
+    def __init__(self, output_dim):
+        super(Subtask2Model, self).__init__()
+        # network hyper parameters
+        self.dropout_factor = 0.5
+
+        # network architecture
+        self.input = nn.Linear(1028, output_dim)
+
+    def forward(self, meta1, meta2, meta3, meta4):
+        
+        x = torch.cat((
+            F.normalize(meta1, p=2, dim=1),
+            F.normalize(meta2, p=2, dim=1),
+            F.normalize(meta3, p=2, dim=1),
+            F.normalize(meta4, p=2, dim=1)),1)
+        x = F.dropout(x, p=self.dropout_factor)
+        x = torch.sigmoid(self.input(x)) # UserWarning: nn.functional.sigmoid is deprecated. Use torch.sigmoid instead.
+        return x
+
