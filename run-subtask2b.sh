b15f86b99b429a24e6562f36feabe38c2accf201
László Király <laszlo.t.kiraly@gmail.com>
2020-02-06 01:30:20 +0100

    paper default settings for block_step and batch_size

--- run-subtask2b.sh
+++ run-subtask2b.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd step1
-python train_subtask2.py --batch_size 5 --block_step 20 --dataset discogs --num_classes 315  
-python train_subtask2.py --batch_size 5 --block_step 20 --dataset lastfm --num_classes 327  
-python train_subtask2.py --batch_size 5 --block_step 20 --dataset allmusic --num_classes 766  
-python train_subtask2.py --batch_size 5 --block_step 20 --dataset tagtraum --num_classes 296  
+python train_subtask2.py --batch_size 32 --block_step 50000 --dataset discogs --num_classes 315  
+python train_subtask2.py --batch_size 32 --block_step 50000 --dataset lastfm --num_classes 327  
+python train_subtask2.py --batch_size 32 --block_step 50000 --dataset allmusic --num_classes 766  
+python train_subtask2.py --batch_size 32 --block_step 50000 --dataset tagtraum --num_classes 296  
 cd ..
